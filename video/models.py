3df70c055c72b7668e0a22d2a5dca137664ab3e5
seethersan <carlos_jcez@hotmail.com>
2020-08-25 20:54:33 -0500

    added ProgresoVideo model

--- video/models.py
+++ video/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from curso.models import Curso
+from usuario.models import Usuario
 
-# Create your models here.
+class ProgresoVideo(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    minutos_rep_diaria = models.IntegerField()
+    id_curso = models.ForeignKey(to=Curso, on_delete=models.CASCADE)
+    id_usuario = models.ForeignKey(to=Usuario, on_delete=models.CASCADE)
