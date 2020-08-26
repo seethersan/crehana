263c750242d21d968cb177cd1bf71dee345ae7cf
seethersan <carlos_jcez@hotmail.com>
2020-08-25 20:54:18 -0500

    added Usuario and Inscripcion models

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from curso.models import Curso
 
-# Create your models here.
+class Usuario(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    email = models.EmailField(max_length=254)
+
+class Inscripcion(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    id_usuario = models.ForeignKey(to=Usuario, on_delete=models.CASCADE)
+    id_curso = models.ForeignKey(to=Curso, on_delete=models.CASCADE)
